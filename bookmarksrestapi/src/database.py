a38478c90c1189297030249df6285b278536e347
PayamAnari <anari.p62@gmail.com>
2024-03-03 15:30:08 +0100

    Modify __init__

--- bookmarksrestapi/src/database.py
+++ bookmarksrestapi/src/database.py
@@ -9,12 +9,12 @@ import random
 db = SQLAlchemy()
 
 class User(db.Model):
-    id=db.column(db.Integer, primary_key=True)
-    username=db.column(db.String(80), unique=True, nullable=False)
-    email=db.column(db.String(80), unique=True, nullable=False)
-    password=db.column(db.Text(80), unique=True, nullable=False)
-    created_at=db.column(db.DateTime, default=datetime.now())
-    updated_at=db.column(db.DateTime, onupdate=datetime.now())
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(80), unique=True, nullable=False)
+    email=db.Column(db.String(80), unique=True, nullable=False)
+    password=db.Column(db.Text(80), unique=True, nullable=False)
+    created_at=db.Column(db.DateTime, default=datetime.now())
+    updated_at=db.Column(db.DateTime, onupdate=datetime.now())
     bookmarks=db.relationship("bookmark", backref="user")
 
 
@@ -23,14 +23,14 @@ class User(db.Model):
 
 
 class Bookmark(db.Model):
-    id=db.column(db.Integer, primary_key=True)
-    body=db.column(db.Text, nullable=False)
-    url=db.column(db.Text, nullable=False)
-    short_url=db.column(db.String(3), nullable=False)
-    visits=db.column(db.Integer, default=0)
-    user_id=db.column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    created_at=db.column(db.DateTime, default=datetime.now())
-    updated_at=db.column(db.DateTime, onupdate=datetime.now())
+    id=db.Column(db.Integer, primary_key=True)
+    body=db.Column(db.Text, nullable=False)
+    url=db.Column(db.Text, nullable=False)
+    short_url=db.Column(db.String(3), nullable=False)
+    visits=db.Column(db.Integer, default=0)
+    user_id=db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    created_at=db.Column(db.DateTime, default=datetime.now())
+    updated_at=db.Column(db.DateTime, onupdate=datetime.now())
 
     
     def generate_short_characters(self):
@@ -40,7 +40,7 @@ class Bookmark(db.Model):
         link=self.query.filter_by(short_url=picked_chars).first()
 
         if link:
-            pass
+            self.generate_short_characters()
 
         else:
             return picked_chars

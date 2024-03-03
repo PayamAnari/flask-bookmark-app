36cbf54fcc996df797675a0f7b19114d2522787e
PayamAnari <anari.p62@gmail.com>
2024-03-03 14:26:18 +0100

    Create auth.py

--- bookmarksrestapi/src/__init__.py
+++ bookmarksrestapi/src/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 
 def create_app(test_config=None):
@@ -9,18 +10,10 @@ def create_app(test_config=None):
     if test_config is None:
         
         app.config.from_mapping(
-             SECRET_KEY="dev"
+             SECRET_KEY=os.environ.get
+             ("SECRET_KEY"),
         )
     else:
         app.config.from_mapping(test_config)
-
-
-    @app.get("/")
-    def index():
-        return "Hello, world!"
-    
-    @app.get("/hello")
-    def say_hello():
-        return {"message": "Hello, world!"}
-        
+      
     return app

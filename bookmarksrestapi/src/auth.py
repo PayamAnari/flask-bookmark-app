4aa7d367b4b95b89fc8573150411f15459ec5c07
PayamAnari <anari.p62@gmail.com>
2024-03-08 13:23:50 +0100

    Add refresh token to swagger

--- bookmarksrestapi/src/auth.py
+++ bookmarksrestapi/src/auth.py
@@ -91,8 +91,9 @@ def me():
     }), HTTP_200_OK
     
 
-@auth.get("/token/refresh")
+@auth.post("/token/refresh")
 @jwt_required(refresh = True)
+@swag_from('./docs/auth/refresh.yaml')
 def refresh_users_token():
     identity = get_jwt_identity()
     access = create_access_token(identity=identity)
